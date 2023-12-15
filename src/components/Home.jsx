e36b30e74373bab732b10bd12cad5b673f532a45
Abdulrahman Bello <belloabdulrahman13@gmail.com>
2023-12-15 13:40:38 -0800

    Animations setup

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,15 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect} from 'react'
 import WordSwitch from './WordSwitch'
 import Aos from 'aos'
 import "aos/dist/aos.css";
 import data from '../data'
 const Home = () => {
-  const [reveal, setReveal] = useState(false);
-
-  useEffect(()=>{
-    const delay = 3000;
-    const revealTimeout = setTimeout(() => {
-      setReveal(true);
-    }, delay);
-    return ()=> clearTimeout(revealTimeout)
-  }, []);
-
   useEffect(()=>{
     Aos.init({duration: 1000});
   }, []);
   return (
     <>
-    { reveal ?
-    <section data-aos="fade-up"  className={`h-screen p-2 w-full  ${reveal ? 'opacity-100' : 'opacity-0'} md:px-28 lg:px-32 section fade-up text-content bg-bg`}>
+    <section data-aos="fade-right"  className={`h-screen p-2 w-full md:px-28 lg:px-32 section fade-up text-content bg-bg relative`}>
         <img src="/public/Drag Logo.png" className=' mx-4 mt-4 hidden dark:block' alt="logo" width="40px"/>
         <img src="/public/DragLogo.png" className=' mx-4 xl:mt-4 dark:hidden block' alt="logo" width="40px"/>
         <div className='h-full w-full absolute'>
@@ -33,17 +22,16 @@ const Home = () => {
                 </div>
             </div>
             <div className='mx-8 mt-4 py-12 lg:w-2/3 lg:pt-12 w-1/2'>
-            <h2 className='text-4xl lg:text-5xl font-bold text-clip mb-4 lg:mb-6'>HE IS {data.name}</h2>
-            <h2 className='text-4xl font-bold text-clip lg:text-5xl lg:mb-6'>HE IS A FRONTEND <span ><WordSwitch/></span></h2>
+            <h2 className='text-4xl lg:text-5xl font-bold text-clip mb-4 lg:mb-6'>I AM {data.name}</h2>
+            <h2 className='text-4xl font-bold text-clip lg:text-5xl lg:mb-6'>I AM A FRONTEND <span ><WordSwitch/></span></h2>
             </div>
             <img src="/src/Long Arrow down.svg" width="90px" className='absolute xl:bottom-32 bottom-16 right-8 xl:right-64 down-arrow' alt="" />
         </div>
-    </section> :
-    <div>
-    <h2 className='h-screen p-2 w-full text-3xl dark:block hidden lg:text-6xl'>Wait for Mylo... <img src="/public/Mylo Head Dark.png" alt=""  width="50px"/> </h2>
-    <h2 className='h-screen p-2 w-full text-3xl lg:text-6xl dark:hidden block'>Wait for Mylo... <img src="/public/Mylo Head Light.png" className='dark:hidden block' alt=""  width="50px"/> </h2>
-    </div>
-}
+      <div className='absolute flex flex-col items-center left-24 lg:left-72 placard-home -top-5 h-full w-full'>
+        <div className='lg:h-3/5 h-2/5 bg-content lg:w-4 w-2'></div>
+        <div className='lg:w-1/5 w-1/4 lg:h-28 h-14 lg:py-9 py-3 text-center text-xl lg:text-4xl bg-content text-bg rounded-xl border-gray-200 border-4'>Welcome</div>
+      </div>
+    </section>
     </>
   )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -35,10 +35,13 @@ const Projects = () => {
     return <Project title={carouselElement.title} key={carouselElement.id} description={carouselElement.description} url={carouselElement.url}/>
   })
   return (
-    <section data-aos="fade-right"  className={`h-screen border-t-4 border-gray-200 md:px-28 lg:px-32  px-8 lg:flex lg:flex-row section bg-bg text-content`}>
-        <div className=''>
+    <section data-aos="fade-right"  className={`h-screen border-t-4 border-gray-200 md:px-28 lg:px-32 gap-6  px-8 lg:flex lg:flex-row section bg-bg text-content relative`}>
+        <div className='flex flex-col gap-0'>
+          <h3 className='p-4 lg:text-xl text-content rounded-xl w-full -mb-64 placard-project lg:mt-8 border-content border-4'> I've built awesome projects and solutions over the years ranging from marketing websites to fintech apps which provide elegant and accesible user experiences.</h3>
+          <div className=''>
             <img src="/public/assets/Ideas to Projects.png" className=' mx-2 mt-96 rotate-12 hidden lg:block lg:dark:hidden' alt="logo" width="300px"/>
             <img src="/public/assets/Ideas to Projects Light.png" className=' mx-2 mt-96 rotate-12 hidden lg:hidden lg:dark:block' alt="logo" width="300px"/>
+          </div>
         </div>
          <div className='bg-gray-200 h-2/3 rounded-xl lg:w-2/3 lg:my-16 lg:h-4/5 lg:ml-auto px-6 flex flex-col justify-center mt-48 relative lg:p-20'>
          <Slider {...settings}>
