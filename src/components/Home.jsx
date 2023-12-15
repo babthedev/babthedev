05f2c98d30ad613df6a3bee30562d3ddcc5cb8b5
Abdulrahman Bello <belloabdulrahman13@gmail.com>
2023-12-15 11:44:40 -0800

    Image path edited

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,15 +21,14 @@ const Home = () => {
     <>
     { reveal ?
     <section data-aos="fade-up"  className={`h-screen p-2 w-full  ${reveal ? 'opacity-100' : 'opacity-0'} md:px-28 lg:px-32 section fade-up text-content bg-bg`}>
-        <img src="/src/assets/Drag Logo.png" className=' mx-4 mt-4 hidden dark:block' alt="logo" width="40px"/>
-        <img src="/src/assets/DragLogo.png" className=' mx-4 xl:mt-4 dark:hidden block' alt="logo" width="40px"/>
+        <img src="/public/assets/Drag Logo.png" className=' mx-4 mt-4 hidden dark:block' alt="logo" width="40px"/>
+        <img src="/public/assets/DragLogo.png" className=' mx-4 xl:mt-4 dark:hidden block' alt="logo" width="40px"/>
         <div className='h-full w-full absolute'>
             <div className='w-44 lg:w-52 lg:h-60 border-content border-2 h-52 relative mx-8 my-14'>
-                <img src="https://ibb.co/FxnNKKc" alt="dots" width="50px" className='absolute -top-4 -left-4 dark:hidden'/>
-                <a href="https://imgbb.com/"><img src="https://i.ibb.co/Lkvb88B/dotted.png" className='absolute -top-4 -left-4 dark:hidden' alt="dotted" border="0" width="50px" /></a>
-                <img src="/src/assets/https://ibb.co/FxnNKKc" alt="dots" width="50px" className='absolute -bottom-10 -right-8 dark:hidden'/>
-                <img src="/src/assets/DotSquared.jpg" alt="dots" width="50px" className='absolute -top-4 -left-4 hidden dark:block'/>
-                <img src="/src/assets/DotSquared.jpg" alt="dots" width="50px" className='absolute -bottom-10 -right-8 hidden dark:block'/>
+                <img src="/public/assets/dotted.png" alt="dots" width="50px" className='absolute -bottom-10 -right-8 dark:hidden'/>
+                <img src="/public/assets/dotted.png" alt="dots" width="50px" className='absolute -bottom-10 -right-8 dark:hidden'/>
+                <img src="/public/assets/DotSquared.jpg" alt="dots" width="50px" className='absolute -top-4 -left-4 hidden dark:block'/>
+                <img src="/public/assets/DotSquared.jpg" alt="dots" width="50px" className='absolute -bottom-10 -right-8 hidden dark:block'/>
                 <div className='w-full h-full bg-gray-200 border-3 border-black absolute top-4 left-4'>
                 </div>
             </div>
@@ -41,8 +40,8 @@ const Home = () => {
         </div>
     </section> :
     <div>
-    <h2 className='h-screen p-2 w-full text-3xl dark:block hidden lg:text-6xl'>Wait for Mylo... <img src="/src/assets/Mylo Head Dark.png" alt=""  width="50px"/> </h2>
-    <h2 className='h-screen p-2 w-full text-3xl lg:text-6xl dark:hidden block'>Wait for Mylo... <img src="/src/assets/Mylo Head Light.png" className='dark:hidden block' alt=""  width="50px"/> </h2>
+    <h2 className='h-screen p-2 w-full text-3xl dark:block hidden lg:text-6xl'>Wait for Mylo... <img src="/public/assets/Mylo Head Dark.png" alt=""  width="50px"/> </h2>
+    <h2 className='h-screen p-2 w-full text-3xl lg:text-6xl dark:hidden block'>Wait for Mylo... <img src="/public/assets/Mylo Head Light.png" className='dark:hidden block' alt=""  width="50px"/> </h2>
     </div>
 }
     </>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -37,8 +37,8 @@ const Projects = () => {
   return (
     <section data-aos="fade-right"  className={`h-screen border-t-4 border-gray-200 md:px-28 lg:px-32  px-8 lg:flex lg:flex-row section bg-bg text-content`}>
         <div className=''>
-            <img src="/src/assets/Ideas to Projects.png" className=' mx-2 mt-96 rotate-12 hidden lg:block lg:dark:hidden' alt="logo" width="300px"/>
-            <img src="/src/assets/Ideas to Projects Light.png" className=' mx-2 mt-96 rotate-12 hidden lg:hidden lg:dark:block' alt="logo" width="300px"/>
+            <img src="/public/assets/Ideas to Projects.png" className=' mx-2 mt-96 rotate-12 hidden lg:block lg:dark:hidden' alt="logo" width="300px"/>
+            <img src="/public/assets/Ideas to Projects Light.png" className=' mx-2 mt-96 rotate-12 hidden lg:hidden lg:dark:block' alt="logo" width="300px"/>
         </div>
          <div className='bg-gray-200 h-2/3 rounded-xl lg:w-2/3 lg:my-16 lg:h-4/5 lg:ml-auto px-6 flex flex-col justify-center mt-48 relative lg:p-20'>
          <Slider {...settings}>
